6c8398cfd58d70e0a5b5559cdb4f0e5f0b467b84
rotem <rotemgrim@gmail.com>
2023-11-06 13:06:14 +0200

    add configs

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,39 @@
+if true then
+  return {}
+end
+
+return {
+  { "rcarriga/nvim-notify", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false },
+  -- {
+  --   "folke/noice.nvim",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   opts = {
+  --     char = "▏",
+  --   },
+  -- },
+  {
+    "echasnovski/mini.indentscope",
+    opts = { symbol = "▏" },
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = "BufRead",
+    config = true,
+  },
+  "Bekaboo/dropbar.nvim",
+  { "SmiteshP/nvim-navic", enabled = false },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      theme = "catppuccin",
+      options = {
+        component_separators = " ",
+        section_separators = { left = "", right = "" },
+      },
+    },
+  },
+}
